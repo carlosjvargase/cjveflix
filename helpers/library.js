469651dd5ba0b28d4d1f1beff6107c43ed6d5488
Carlos J Vargas E <104727028+carlosjvargase@users.noreply.github.com>
2022-07-19 17:28:24 -0500

    Add files via upload

---
+++ helpers/library.js
@@ -0,0 +1,405 @@
+import titanicS from "../images/titanicScene.png";
+import tita from "../images/tit.png";
+import movieLogo from "../images/movieLogo.png";
+import spidey from "../images/spidey.png";
+import jumanji from "../images/jumanji.png";
+import wolf from "../images/wolf.png";
+import wolfScene from "../images/wolfScene.png"; 
+import wolfLogo from "../images/wolfLogo.png";
+import mrs from "../images/mrs.png";
+import mrsScene from "../images/mrsScene.png";
+import mrsLogo from "../images/mrsLogo.png";
+import hob from "../images/hob.png";
+import spideyLogo from "../images/spideyLogo.png";
+import spideyScene from "../images/spideyScene.png";
+import jam from "../images/jam.png";
+import jamLogo from "../images/jamLogo.png";
+import jamScene from "../images/jamScene.png";
+import bayw from "../images/bayw.png";
+import shrek from "../images/shrek.png";
+import lucy from "../images/lucy.png";
+import topgu from "../images/topgu.png";
+import imit from "../images/imit.png";
+import reven from "../images/reven.png";
+import tomb from "../images/tomb.png";
+import amaz from "../images/amaz.png";
+import bigs from "../images/bigs.png";
+import ted from "../images/ted.png";
+import amerp from "../images/amerp.png";
+import fightClubS from "../images/fightClubSc.png";
+import fightClub from "../images/fightClub.png";
+import fightLogo from "../images/fightLogo.png";
+import matrix from "../images/matrix.png";
+import matrixS from "../images/matrixScene.png";
+import matrixLogo from "../images/matrixLogo.png";
+import citya from "../images/citya.png";
+import cityaScene from "../images/cityaScene.png";
+import cityaLogo from "../images/cityaLogo.png";
+import avatar from "../images/avatar.png";
+import avatarScene from "../images/avatarScene.png";
+import avatarLogo from "../images/avatarLogo.png";
+import jumanjiScene from "../images/jumanjiScene.png";
+import jumanjiLogo from "../images/jumanjiLogo.png";
+import hustle from "../images/hustle.png";
+import hustleScene from "../images/hustleScene.png";
+import hustleLogo from "../images/hustleLogo.png";
+import birdbox from "../images/birdbox.png";
+import birdboxScene from "../images/birdboxScene.png";
+import birdboxLogo from "../images/birdboxLogo.png";
+import iaml from "../images/iaml.png";
+import iamlScene from "../images/iamlScene.png";
+import iamlLogo from "../images/iamlLogo.png";
+import shrekScene from "../images/shrekScene.png";
+import shrekLogo from "../images/shrekLogo.png";
+import revenScene from "../images/revenScene.png";
+import revenLogo from "../images/revenLogo.png";
+import hobScene from "../images/hobScene.png";
+import hobLogo from "../images/hobLogo.png";
+import imitScene from "../images/imitScene.png";
+import imitLogo from "../images/imitLogo.png";
+import topguScene from "../images/topguScene.png";
+import topguLogo from "../images/topguLogo.png";
+import lucyScene from "../images/lucyScene.png";
+import lucyLogo from "../images/lucyLogo.png";
+import baywScene from "../images/baywScene.png";
+import baywLogo from "../images/baywLogo.png";
+import tombScene from "../images/tombScene.png";
+import tombLogo from "../images/tombLogo.png";
+import amazScene from "../images/amazScene.png";
+import amazLogo from "../images/amazLogo.png";
+import bigsScene from "../images/bigsScene.png";
+import bigsLogo from "../images/bigsLogo.png";
+import tedScene from "../images/tedScene.png";
+import tedLogo from "../images/tedLogo.png";
+import amerpScene from "../images/amerpScene.png";
+import amerpLogo from "../images/amerpLogo.png";
+import titanicVideo from "../videos/titanic.mp4";
+import shrekVideo from "../videos/shrek.mp4";
+import spidermanhomecoming from "../videos/spidermanhomecoming.mp4";
+import amazVideo from "../videos/theamazingspiderman.mp4";
+import avatarV from "../videos/avatar.mp4";
+import baywV from "../videos/bayw.mp4";
+import cityofangels from "../videos/cityofangels.mp4";
+import fightclubV from "../videos/fightclub.mp4";
+import hustleV from "../videos/hustle.mp4";
+import iamlegend from "../videos/iamlegend.mp4";
+import jumanjiV from "../videos/jumanji.mp4";
+import lucyV from "../videos/lucy.mp4";
+import  matrixV from "../videos/matrix.mp4";
+import mrandmrssmith from "../videos/mrandmrssmith.mp4";
+import spaceJamV from "../videos/SpaceJam2.mp4";
+import tedV from "../videos/ted.mp4";
+import birdboxV from "../videos/birdbox.mp4"
+import thebigshortV from "../videos/thebigshort.mp4"
+import thehobbitV from "../videos/thehobbit.mp4"
+import theimitationgameV from "../videos/theimitationgame.mp4"
+import therevenantV from "../videos/birdbox.mp4"
+import thewolfofwallstreetV from "../videos/thewolfofwallstreet.mp4"
+import tombraiderV from "../videos/tombraider.mp4" 
+import topgunmaverickV from "../videos/topgunmaverick.mp4"
+
+export const movies = [
+  {
+    Movie: fightclubV,
+    Thumnbnail: fightClub,
+    Scene: fightClubS,
+    Logo: fightLogo,
+    Name: "Fight Club",
+    Description:
+      "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+      Genre: "Drama, Action",
+    Actors: "Brad Pitt, Edward Norton & Helena Bonham Carter",
+    Year: "1999",
+    Duration: "2h 19min",
+  },
+  {
+    Movie: hustleV,
+    Thumnbnail: hustle,
+    Scene: hustleScene,
+    Logo: hustleLogo,
+    Name: "Hustle",
+    Description: "The film stars Adam Sandler as an NBA scout who discovers a raw but talented player in Spain (Juancho Hernangómez) and tries to prepare him for the NBA draft.",
+    Genre: "American Sport Drama",
+    Actors: "Adam Sandler, Juancho Hernangomez, Ben Foster",
+    Year: "2022",
+    Duration: "1h 57min",
+  },
+  {
+    Movie: matrixV,
+    Thumnbnail: matrix,
+    Scene: matrixS,
+    Logo: matrixLogo,
+    Name: "Matrix",
+    Description:
+      "It depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix, which intelligent machines have created to distract humans while using their bodies as an energy source.",
+    Genre: "Action, Drama",
+    Actors: "Keanu Reeves, Carrie-Anne Moss, Laurence Fishburne",
+    Year: "1999",
+    Duration: "2h 16min",
+  },
+  {
+    Movie: avatarV,
+    Thumnbnail: avatar,
+    Scene: avatarScene,
+    Logo: avatarLogo,
+    Name: "Avatar",
+    Description: "Avatar takes us to the amazing world of Pandora, where a man embarks on an epic journey of adventure and love, ultimately fighting to save the unique place he has learned to call home.",
+    Genre: "Action",
+    Actors: "Sam Worthington, Zoe Saldana, Sigourne Weaver, Stephen Lang.",
+    Year: "2009",
+    Duration: "2h 41min",
+  },
+  {
+    Movie: iamlegend,
+    Thumnbnail: iaml,
+    Scene: iamlScene,
+    Logo: iamlLogo,
+    Name: "I am Legend",
+    Description: "It is set in New York City after a virus, which was originally created to cure cancer, has wiped out most of mankind, leaving Neville as the last human in New York, other than nocturnal mutants. Neville is immune to the virus, and he works to develop a cure while defending himself against the hostile mutants.",
+    Genre: "Thriller, Action",
+    Actors: "Will Smith, Alice Braga, Charlie Tahan",
+    Year: "2007",
+    Duration: "1h 41min",
+  },
+  {
+    Movie: spidermanhomecoming,
+    Thumnbnail: spidey,
+    Scene: spideyScene,
+    Logo: spideyLogo,
+    Name: "Spiderman Homecoming",
+    Description: "Peter Parker tries to stop the Vulture from selling weapons made with advanced Chitauri technology while trying to balance his life as an ordinary high school student.",
+    Genre: "Action, Superhero, Comedy",
+    Actors: "Tom Holland, Zendeya",
+    Year: "2017",
+    Duration: "2h 13min",
+  },
+  {
+    Movie: birdboxV,
+    Thumnbnail: birdbox,
+    Scene: birdboxScene,
+    Logo: birdboxLogo,
+    Name: "Bird Box",
+    Description: "The film follows the character Malorie Hayes, as she tries to protect herself and two children from entities which cause people who look at them to die by suicide.",
+    Genre: "Horro Thriller",
+    Actors: "Sandra Bullock, Trevante Rhodes, Sereh Paulson, John Malkovich",
+    Year: "2018",
+    Duration: "2h 4min",
+  },
+  {
+    Movie: jumanjiV,
+    Thumnbnail: jumanji,
+    Scene: jumanjiScene,
+    Logo: jumanjiLogo,
+    Name: "Jumanji: The Next Level",
+    Description: "The film's plot takes place two years after Welcome to the Jungle, in which the same group of teenagers, along with an old friend and two unwitting additions, become trapped in Jumanji once again. There, they all find themselves facing new problems and challenges with both old and new avatars while having to save the land from a new villain to escape.",
+    Genre: "Fantasy, Adventure, Comedy",
+    Actors: "Dwayne Johnson, Karen Gillan, Jack Black, Kevin Hart, Awkwafina, Danny DeVito",
+    Year: "2019",
+    Duration: "2h 3min",
+  },
+  {
+    Movie: cityofangels,
+    Thumnbnail: citya,
+    Scene: cityaScene,
+    Logo: cityaLogo,
+    Name: "City of Angel",
+    Description: "City of Angels tells the story of an angel who falls in love with a mortal woman, and wishes to become human in order to be with her. With the guidance of a man who has already made the transition from immortality, the angel falls and discovers the human experience.",
+    Genre: "Romance, Drama",
+    Actors: "Nicolas Cage, Meg Ryan, Andre Braugher, Dennis Franz",
+    Year: "1998",
+    Duration: "1h 54min",
+  },
+  {
+    Movie: thewolfofwallstreetV,
+    Thumnbnail: wolf,
+    Scene: wolfScene,
+    Logo: wolfLogo,
+    Name: "Wolf Of Wallstreet",
+    Description: "In 1987, Jordan Belfort takes an entry-level job at a Wall Street brokerage firm. By the early 1990s, while still in his 20s, Belfort founds his own firm, Stratton Oakmont. Together with his trusted lieutenant and a merry band of brokers, Belfort makes a huge fortune by defrauding wealthy investors out of millions. However, while Belfort and his cronies partake in a hedonistic brew of sex, drugs and thrills, the SEC and the FBI close in on his empire of excess.",
+    Genre: "Drama, Comedy",
+    Actors: "Leonardo DiCaprio, Jonah Hill, Margot Robbie, Matthew McConaughey",
+    Year: "2013",
+    Duration: "3h",
+  },
+  {
+    Movie: mrandmrssmith,
+    Thumnbnail: mrs,
+    Scene: mrsScene,
+    Logo: mrsLogo,
+    Name: "Mr and Mrs Smith",
+    Description: "John and Jane Smith, a couple in a stagnating marriage, live a deceptively mundane existence. However, each has been hiding a secret from the other: they are assassins working for adversarial agencies. When they are both assigned to kill the same target, Benjamin Danz, the truth comes to the surface. Finally free from their cover stories, they discover that they have been assigned to kill each other, sparking a series of explosive attacks.",
+    Genre: "Action, Romance",
+    Actors: "Bradd Pitt, Angelina Jolie, Vince Vaughn, Adam Brody",
+    Year: "2005",
+    Duration: "2h 6min",
+  },
+  {
+    Movie: spaceJamV,
+    Thumnbnail: jam,
+    Scene: jamScene,
+    Logo: jamLogo,
+    Name: "Space jam 2",
+    Description: "A rogue artificial intelligence kidnaps the son of famed basketball player LeBron James, who then has to work with Bugs Bunny to win a basketball game.",
+    Genre: "Comedy",
+    Actors: "LeBron James, Zendaya, Michael B. Jordan",
+    Year: "2021",
+    Duration: "2h 25 min",
+  },
+  {
+    Movie: thehobbitV,
+    Thumnbnail: hob,
+    Scene: hobScene,
+    Logo: hobLogo,
+    Name: "Hobbit",
+    Description: "Bilbo Baggins lives a simple life with his fellow hobbits in the shire, until the wizard Gandalf arrives and convinces him to join a group of dwarves on a quest to reclaim the kingdom of Erebor. The journey takes Bilbo on a path through treacherous lands swarming with orcs, goblins and other dangers, not the least of which is an encounter with Gollum and a simple gold ring that is tied to the fate of Middle Earth in ways Bilbo cannot even fathom",
+    Genre: "Adventure, Fantasy ",
+    Actors: "Martin Freeman, Ian Mckellen, Andy Serkis, Richard Armitage, Andy Serkis",
+    Year: "2012",
+    Duration: "2h 49min",
+  },
+  {
+    Movie: shrekVideo,
+    Thumnbnail: shrek,
+    Scene: shrekScene,
+    Logo: shrekLogo,
+    Name: "Shrek",
+    Description: "Once upon a time, in a far away swamp, there lived an ogre named Shrek whose precious solitude is suddenly shattered by an invasion of annoying fairy tale characters. They were all banished from their kingdom by the evil Lord Farquaad. Determined to save their home -- not to mention his - Shrek cuts a deal with Farquaad and sets out to rescue Princess Fiona to be Farquaad's bride. Rescuing the Princess may be small compared to her deep, dark secret.",
+    Genre: "Comedy, Fantasy",
+    Actors: "Mike Myers, Eddie Murphy, Cameron Diaz, Chris Miller, Conrad Vemon",
+    Year: "2001",
+    Duration: "1h 30min",
+  },
+  {
+    Movie: therevenantV,
+    Thumnbnail: reven,
+    Scene: revenScene,
+    Logo: revenLogo,
+    Name: "The Revenant",
+    Description: "While exploring the uncharted wilderness in 1823, frontiersman Hugh Glass (Leonardo DiCaprio) sustains life-threatening injuries from a brutal bear attack. When a member (Tom Hardy) of his hunting team kills his young son (Forrest Goodluck) and leaves him for dead, Glass must utilize his survival skills to find a way back to civilization. Grief-stricken and fueled by vengeance, the legendary fur trapper treks through the snowy terrain to track down the man who betrayed him.",
+    Genre: "Western, Adventure",
+    Actors: "",
+    Year: "2015",
+    Duration: "2h 36min",
+  },
+  {
+    Movie: theimitationgameV,
+    Thumnbnail: imit,
+    Scene: imitScene,
+    Logo: imitLogo,
+    Name: "The Imitation Game",
+    Description: "In 1951, two policemen, Nock and Staehl, investigate the mathematician Alan Turing after an apparent break-in at his home. During his interrogation by Nock, Turing tells of his time working at Bletchley Park during the Second World War.",
+    Genre: "War, Drama",
+    Actors: "Benedict Cumberbatch, Keira Knightley, Mathew Goode, Charles Dance, Mark Strong",
+    Year: "2014",
+    Duration: "1h 54min",
+  },
+  {
+    Movie: topgunmaverickV,
+    Thumnbnail: topgu,
+    Scene: topguScene,
+    Logo: topguLogo,
+    Name: "Top Gun: Maverick",
+    Description: "Over thirty years after graduating from TOPGUN, United States Navy Captain Pete Maverick Mitchell is a test pilot. While he has won many honors, repeated insubordination has kept him from flag rank, with his friend and TOPGUN rival Tom Iceman Kazansky repeatedly protecting Maverick from being grounded. ",
+    Genre: "Action, Adventure",
+    Actors: "Tom Cruise, Miles Teller, Val Kilmer, Glen Powel, Jennifer Connelly, Jon Hamm",
+    Year: "2022",
+    Duration: "2h 11min",
+  },
+  {
+    Movie: lucyV,
+    Thumnbnail: lucy,
+    Scene: lucyScene,
+    Logo: lucyLogo,
+    Name: "Lucy",
+    Description: "Lucy is an American studying in Taipei, Taiwan and is tricked into working as a drug mule by her new boyfriend Richard for a South Korean kkangpae and drug lord. Lucy delivers a briefcase to Mr. Jang containing four packets of CPH4, a highly valuable synthetic drug.",
+    Genre: "Action, Sci-fi",
+    Actors: "Scarlett Johansson, Morgan Freeman, Choi Min-sik, Amr Waked",
+    Year: "2014",
+    Duration: "1h 30min",
+  },
+  {
+    Movie: baywV,
+    Thumnbnail: bayw,
+    Scene: baywScene,
+    Logo: baywLogo,
+    Name: "Baywatch",
+    Description: "In Emerald Bay, Florida, Lt. Mitch Buchannon and his team of lifeguards, including second-in-command Stephanie Holden and veteran C.J. Parker, protect the beaches and the bay as part of an elite division known as Baywatch.",
+    Genre: "Action, Comedy",
+    Actors: "Dwayne Johnson, Zac Efron, Kelly Rohrbach, Alexandra Daddario",
+    Year: "2017",
+    Duration: "2h 1min",
+  },
+  {
+    Movie: tombraiderV,
+    Thumnbnail: tomb,
+    Scene: tombScene,
+    Logo: tombLogo,
+    Name: "Tomb Raider",
+    Description: "Following the disappearance of her father, Lord Richard Croft, Lara Croft makes a living as a bike courier. She is arrested after a collision with a police car. Richard's business partner Ana Miller posts her bail and urges Lara to accept her inheritance.",
+    Genre: "Aventure, Action",
+    Actors: "Alicia Vikander, Dominic West, Wlaton Goggins",
+    Year: "2018",
+    Duration: "1h 59min",
+  },
+  {
+    Movie: amazVideo,
+    Thumnbnail: amaz,
+    Scene: amazScene,
+    Logo: amazLogo,
+    Name: "The Amazing Spiderman",
+    Description: "A young Peter Parker discovers that his father Richard Parker's study has been burgled. Peter's parents gather hidden documents, take Peter to the home of his Aunt May and Uncle Ben, then mysteriously depart. Years later, a teenage Peter attends Midtown Science High School; he is intelligent but socially awkward and often bullied. He also has a crush on Gwen Stacy, who returns his feelings.",
+    Genre: "Superheroes, Action, Adventure",
+    Actors: "Andrew Garfield, Emma Stone, Rhys Ifans, Sally Field",
+    Year: "2012",
+    Duration: "2h 16min",
+  },
+  {
+    Movie: thebigshortV,
+    Thumnbnail: bigs,
+    Scene: bigsScene,
+    Logo: bigsLogo,
+    Name: "The Big Short",
+    Description: "The film consists of three separate but concurrent stories, loosely connected by their actions in the years leading up to the 2007 housing market crash.",
+    Genre: "Drama, Comedy",
+    Actors: "Steve Carell, Christian Bale, Ryan Gosling, Brad Pitt",
+    Year: "2015",
+    Duration: "2h 10min",
+  },
+  {
+    Movie: tedV,
+    Thumnbnail: ted,
+    Scene: tedScene,
+    Logo: tedLogo,
+    Name: "Ted",
+    Description: "The film tells the story of John Bennett, a Boston native whose childhood wish brings his teddy bear friend Ted to life. However, in adulthood, Ted prevents John and his girlfriend Lori Collins from moving on with their lives.",
+    Genre: "Comedy, Fantasy",
+    Actors: "Seth MacFarlane, Mark Wahlberg, Mila Kunis",
+    Year: "2012",
+    Duration: "1h 46min",
+  },
+  {
+    Movie: "",
+    Thumnbnail: amerp,
+    Scene: amerpScene,
+    Logo: amerpLogo,
+    Name: "American Pie",
+    Description: "The plot centers on five classmates who attend East Great Falls High. With the sole exception of Stifler, who has already lost his virginity, the youths make a pact to lose their virginity before their high school graduation.",
+    Genre: "Comedy, Romance",
+    Actors: "Janson Biggs, Seann William Scott, Alyson Hannigan, Chris Klein, Tara Reid",
+    Year: "1999",
+    Duration: "1h 35min",
+  },
+  {
+    Movie: titanicVideo,
+    Thumnbnail: tita,
+    Scene: titanicS,
+    Logo: movieLogo,
+    Name: "Titanic",
+    Description:
+      "Staring Kate Winslet and Leonardo DiCaprio. The two play characters who are of different social classes. They fall in love after meeting aboard the ship, but it was not good for a rich girl to fall in love with a poor boy in 1912.",
+    Genre: "Romance, Drama",
+    Actors: "Kate Winslet, Leonardo DiCaprio",
+    Year: "1998",
+    Duration: "3h 14min",
+  },
+];
